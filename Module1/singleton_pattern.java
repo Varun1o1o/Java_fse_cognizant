9a29b25be482cb02cf171c30f384384172f389fa
Gajula Varun <144794316+Varun1o1o@users.noreply.github.com>
2026-06-22 18:36:05 +0530

    Add Logger singleton implementation and test
    Implement singleton pattern for Logger class with test.

---
+++ Module1/singleton_pattern.java
@@ -0,0 +1,33 @@
+class Logger {
+
+    private static Logger instance;
+
+    private Logger() {
+        System.out.println("Logger instance created");
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("Log: " + message);
+    }
+}
+
+public class SingletonTest {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("First message");
+        logger2.log("Second message");
+
+        System.out.println(logger1 == logger2);
+    }
+}
